e6ca804b8f382ab649d3fef50e85cf1180121aa3
Victor Martin Rivera Moreno <102710182+Victor-Martin-Rivera@users.noreply.github.com>
2023-07-27 13:58:22 -0700

    Update MetodosSet.py

--- 14. Pilares POO/MetodosSet.py
+++ 14. Pilares POO/MetodosSet.py
@@ -12,15 +12,26 @@ class A():
 
     @cuenta.setter
     def cuenta(self, cuenta):
+        #Valor que va a ser seteado
         self._cuenta = cuenta 
 
+    #Get Contador
     @property 
     def contador(self):
         return self._contador
     
+    @contador.setter
+    def contador(self, contador):
+        self._contador = contador
+    
 a = A()
-#Mando a llamar el metodo cuenta con valor 0
+#Mando a llamar el metodo cuenta con valor 0 (get)
 print(a.cuenta)
+#Se modifica el valor del set
+a.cuenta = 20
+print(a.cuenta)
+print(a.contador)
+a.contador = 10
 print(a.contador)
 # a._contador = 10
 # print(a._cuenta)
